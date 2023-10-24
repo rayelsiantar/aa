f54a39667773270494886aafc2a0a5ce0259fe40
telepuqi <86878198+telepuqi@users.noreply.github.com>
2023-10-24 13:16:30 +0700

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -29,10 +29,7 @@ def start_button(client):
     if FORCE_SUB_CHANNEL and not FORCE_SUB_GROUP:
         buttons = [
             [
-                InlineKeyboardButton(text="ᴄʜᴀɴɴᴇʟ", url=client.invitelink),
-            ],
-            [
-                InlineKeyboardButton(text="ʜᴇʟᴘ & ᴄᴏᴍᴍᴀɴᴅs", callback_data="help"),
+                InlineKeyboardButton(text="ᴛᴇɴᴛᴀɴɢ ꜱᴀʏᴀ", callback_data="help"),
                 InlineKeyboardButton(text="ᴛᴜᴛᴜᴘ", callback_data="close"),
             ],
         ]
